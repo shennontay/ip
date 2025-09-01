96d1a5cc088c240b3dd3c1684bcd05de3b9fc202
shennontay <shennontay@gmail.com>
2025-09-01 16:05:26 +0800

    implement echo function

--- src/main/java/nomnom.java
+++ src/main/java/nomnom.java
@@ -1,11 +1,33 @@
+import java.util.Scanner;
+
 public class nomnom {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
-        System.out.println("Hello! I'm nomnom.\n" + "What can I do for you?\n\n" + "Bye. Hope to see you again soon!");
+        System.out.println("_".repeat(50));
+
+        String asciiArt = """
+                 ,--,--,  ,---. ,--,--,--.,--,--,  ,---. ,--,--,--.
+                 |      \\| .-. ||        ||      \\| .-. ||        |
+                 |  ||  |' '-' '|  |  |  ||  ||  |' '-' '|  |  |  |
+                 `--''--' `---' `--`--`--'`--''--' `---' `--`--`--'
+                """;
+        System.out.println(asciiArt);
+
+        System.out.println("Hello! I'm nomnom.\nWhat can I do for you?");
+        System.out.println("_".repeat(50) + '\n');
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println("_".repeat(50) + '\n');
+            System.out.println(input);
+            System.out.println("_".repeat(50) + '\n');
+        }
+
+        System.out.println("_".repeat(50) + '\n');
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_".repeat(50) + '\n');
     }
 }
